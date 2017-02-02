a79ceec2697854af8819840e7b2d19a89ab9ecb6
Dmytro Omelchuk <omeldm@gmail.com>
2017-02-02 23:14:02 +0200

    Update 2017-02-02T21:13:59.919Z

--- main.js
+++ main.js
@@ -6,6 +6,18 @@ function main () {
 function block (argument) {
   // body...
 }
-function block (argument) {
-  // body...
-}
+$(document).ready(function() {
+  $('.menu__icon_close').hide();
+
+  $('.menu').click(function() {
+    $('.navigation-container_small').toggleClass('navigation-container_active');
+
+    if($('.menu__icon_burger').is(":visible")) {
+      $('.menu__icon_burger').hide();
+      $('.menu__icon_close').show();
+    } else {
+      $('.menu__icon_burger').show();
+      $('.menu__icon_close').hide();
+    }
+  });
+});
