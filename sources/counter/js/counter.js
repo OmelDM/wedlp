62b50a71757284b7f776fd1dfc7aa184b06c7873
Dmytro Omelchuk <omeldm@gmail.com>
2017-02-07 21:54:50 +0200

    Implemented countdown functionality

--- sources/counter/js/counter.js
+++ sources/counter/js/counter.js
@@ -1,3 +1,11 @@
-function block (argument) {
-  // body...
-}
+$(document).ready(function() {
+  function makeTemplate(period) {
+    return '<li class="counter__item"><div class="counter__value"><span>{'+period+'n}</span></div><div class="counter__measurement">{'+period+'l}</div></li>'
+  }
+
+  $('.counter__time').countdown({
+    until: new Date(2017, 8, 16),
+    format: 'DHMS',
+    layout: makeTemplate('d') + makeTemplate('h') + makeTemplate('m') + makeTemplate('s')
+  });
+});
