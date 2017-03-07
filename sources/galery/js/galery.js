f4965faa1e4e2664f473d9dfd4073c56f7f29ce7
Dmytro Omelchuk <omeldm@gmail.com>
2017-03-07 16:26:40 +0200

    Marked up galery section

---
+++ sources/galery/js/galery.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+  $('[data-magnigic-popup]').magnificPopup({
+    type: 'image',
+  });
+
+  $('[data-tag-target]').click(function() {
+    $('[data-tag-current]').removeAttr('data-tag-current');
+    $(this).attr('data-tag-current', 'true');
+    var targetTag = $(this).data('tag-target');
+    if ('tag_all' === targetTag) {
+      $('[data-tag]').show();
+    } else {
+      $('[data-tag='+targetTag+']').show();
+      $('[data-tag]').not('[data-tag='+targetTag+']').hide();
+    }
+  });
+
+});
