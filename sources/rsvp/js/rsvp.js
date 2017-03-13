cc795f22a03c9a8bb766f7118a5c38434851d565
Dmytro Omelchuk <omeldm@gmail.com>
2017-03-13 15:04:13 +0200

    Added events selection for rsvp

--- sources/rsvp/js/rsvp.js
+++ sources/rsvp/js/rsvp.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
-  $('#events').selectric();
+
+  selectAll()
+
+  $('[data-select-event]').change(function(event) {
+    if (event.target.value === 'all') {
+      selectAll();
+    } else {
+    //  $('[data-select-event] option[name="all"]').removeAttr('selected');
+    }
+  });
+
+  function selectAll() {
+    $('[data-select-event] option').each(function(index, element) {
+      $(element).prop('selected', 'true');
+    });
+  }
+
 });
