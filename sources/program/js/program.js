3db17dc8d7226abc0760801936194d6c0e584fd6
Dmytro Omelchuk <omeldm@gmail.com>
2017-04-08 21:21:32 +0300

    Made first event opened to show possibility to toggle events

--- sources/program/js/program.js
+++ sources/program/js/program.js
@@ -2,4 +2,7 @@ $(document).ready(function() {
   $('[data-accardion-header]').click(function() {
     $(this).next().slideToggle();
   });
+
+  $('[data-accardion-header]').eq(0).next().slideToggle();
+
 });
