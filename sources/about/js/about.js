c061bd640dc82a9945f8cf100d1f5d0d3f0c4712
Dmytro Omelchuk <omeldm@gmail.com>
2017-04-08 21:10:12 +0300

    Added animation

--- sources/about/js/about.js
+++ sources/about/js/about.js
@@ -1,6 +1,2 @@
 $(document).ready(function() {
-
-  var bgUrl = $('.hero-container').data('background-url');
-  $('.hero-container').css('background-image', 'url('+bgUrl+')');
-
 });
--- sources/common.js
+++ sources/common.js
@@ -1,4 +1,11 @@
-function main () {
-  // get filter=filterName
+$(document).ready(function() {
 
-}
+  $('[data-animation]').css('visibility', 'hidden');
+
+  $('[data-animation]').one('inview', function() {
+    var animationType = 'animated ' + $(this).data('animation');
+    $(this).addClass(animationType);
+    $(this).css('visibility', 'visible');
+  })
+
+});
