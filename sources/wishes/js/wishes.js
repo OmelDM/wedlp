2066e5b707e3cb78124b5ab75b45411738af9f6e
Dmytro Omelchuk <omeldm@gmail.com>
2017-04-11 17:21:14 +0300

    Made scroll to add a wish

--- sources/wishes/js/wishes.js
+++ sources/wishes/js/wishes.js
@@ -12,4 +12,11 @@ $(document).ready(function() {
     prevHtml: '<svg class="arrow arrow_prev" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><path d="M35 88.5c-.8 0-1.5-.3-2.1-.9-1.2-1.2-1.2-3.1 0-4.2l35.7-35.7-36-36c-1.2-1.2-1.2-3.1 0-4.2 1.2-1.2 3.1-1.2 4.2 0L77 47.7 37.2 87.6c-.6.6-1.4.9-2.2.9z"/></svg>',
   });
 
+  $('[data-action="addWish"]').click(function() {
+    $.smoothScroll({
+      scrollElement: $('html, body'),
+      scrollTarget: '#rsvp'
+    });
+  });
+
 });
