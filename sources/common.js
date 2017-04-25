ad28aa49e691056b5737853ce3ba2b7a4e590f7c
Dmytro Omelchuk <omeldm@gmail.com>
2017-04-25 11:28:51 +0300

    Corrected avoiding animation for small sizes

--- sources/common.js
+++ sources/common.js
@@ -2,16 +2,15 @@ $(document).ready(function() {
 
   var ANIMATION_MIN_WIDTH = 768;
 
-  if (ANIMATION_MIN_WIDTH < window.innerWidth) {
-    $('[data-animation]').css('visibility', 'hidden');
-  }
+  $('[data-animation]').css('visibility', 'hidden');
 
   $('[data-animation]').one('inview', function() {
     if (ANIMATION_MIN_WIDTH < window.innerWidth) {
       var animationType = 'animated ' + $(this).data('animation');
       $(this).addClass(animationType);
-      $(this).css('visibility', 'visible');
+
     }
+    $(this).css('visibility', 'visible');
   })
 
 });
